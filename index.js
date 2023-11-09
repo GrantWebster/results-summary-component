d2984427e9811f37cd5b1a47644416c4a8d1accd
Grant <webster.g@icloud.com>
2023-11-09 14:48:42 +0200

    Update with finished styling and added index.js to fetch local JSON data

--- index.js
+++ index.js
@@ -4,22 +4,42 @@ const reactionScore = document.getElementById('id-rea-s');
 const memoryScore = document.getElementById('id-mem-s');
 const verbalScore = document.getElementById('id-verb-s');
 const visualScore = document.getElementById('id-vis-s');
-// const icon = document.getElementsByClassName();
 
+const reactionLabel = document.getElementById('rea-label');
+const memoryLabel = document.getElementById('mem-label');
+const verbalLabel = document.getElementById('ver-label');
+const visualLabel = document.getElementById('vis-label');
 
 
 fetch('./data.json')
-.then(response => response.json())
-.then(jsonResponse => renderData(jsonResponse));
+.then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => {
+  console.log(networkError.message)
+}).then((jsonResponse) => {
+  renderData(jsonResponse);
+})
+
 
 function renderData(jsonResponse) {
   
+  // console.log(jsonResponse);
+
+  reactionLabel.innerHTML = jsonResponse[0].category;
+  memoryLabel.innerHTML = jsonResponse[1].category;
+  verbalLabel.innerHTML = jsonResponse[2].category;
+  visualLabel.innerHTML = jsonResponse[3].category;
+
   reactionScore.innerHTML = jsonResponse[0].score;
   memoryScore.innerHTML = jsonResponse[1].score;
   verbalScore.innerHTML = jsonResponse[2].score;
   visualScore.innerHTML = jsonResponse[3].score;
 
-  totalScore.innerHTML = Math.floor((80 + 92 + 61 + 72) / 4);
+  const calcScore = Math.floor((jsonResponse[0].score + jsonResponse[1].score + jsonResponse[2].score + jsonResponse[3].score) / jsonResponse.length);
+  totalScore.innerHTML = calcScore; 
 
 }
 
